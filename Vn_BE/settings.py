fa36ae68434f1a14a31bffeeafe88a7f3bb76a98
PhilemonBrain <philbrainy@gmail.com>
2020-12-13 11:41:47 +0100

    whitenoise

--- Vn_BE/settings.py
+++ Vn_BE/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 # DEBUG = True
-ALLOWED_HOSTS = ['voicenote-be-api-django-app.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['voicenote-be-api-django-app.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
